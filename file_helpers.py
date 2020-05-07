f8aa6a74098fdb6ef06d1597694c431bb98b386e
Colm Britton <colmjude@gmail.com>
2020-05-07 15:30:24 +0100

    Generate index to point at boundaries

--- file_helpers.py
+++ file_helpers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import csv
 import json
 import requests
 import pandas as pd
@@ -28,3 +29,39 @@ def save_geojson(data, path_to_file):
     with open(path_to_file, "w") as f:
         json.dump(data, f)
 
+
+# need a method that returns dict as well as adds k:v
+# useful in list comprehensions
+def update_dict(d, k, val):
+    d[k] = val
+    return d
+
+
+# write json to csv file
+def json_to_csv_file(output_file, data):
+
+    print(f"Write data to {output_file}")
+    # now we will open a file for writing
+    data_file = open(output_file, "w")
+    # create the csv writer object
+    csv_writer = csv.writer(data_file, lineterminator="\n")
+    # Counter variable used for writing
+    # headers to the CSV file
+    count = 0
+    for row in data:
+        if count == 0:
+
+            # Writing headers of CSV file
+            header = row.keys()
+            csv_writer.writerow(header)
+            count += 1
+
+        # Writing data of CSV file
+        csv_writer.writerow(row.values())
+
+    data_file.close()
+
+
+def indexed_dict_to_csv(output_file, data, idx_name):
+    data = [update_dict(v, idx_name, k) for k, v in data.items()]
+    json_to_csv_file(output_file, data)
--- split_boundaries.py
+++ split_boundaries.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 
 import os
-from file_helpers import open_json_file, save_geojson, get_csv_as_json
+from file_helpers import open_json_file, save_geojson, get_csv_as_json, indexed_dict_to_csv
 
 
 geojson_template = {
     'type': 'FeatureCollection',
     'features': []
 }
+
+
+boundary_index = {}
 
 
 def get_boundary_type(t):
@@ -18,12 +21,19 @@ def extract_name_from_document_url(url):
     return url.split("/")[-1]
 
 
+def make_boundary_url(stat_geo, typ):
+    return f'https://raw.githubusercontent.com/digital-land/region-collection/master/collection/{typ}/{stat_geo}/index.geojson'
+
+
 def split_boundaries(path, boundary_type):
     boundaries = open_json_file(path)
     for boundary in boundaries['features']:
         data = geojson_template
         data['features'] = [boundary]
         k = boundary['properties']['rgn19cd']
+
+        boundary_index.setdefault(k, {})
+        boundary_index[k][boundary_type] = make_boundary_url(k, boundary_type)
 
         boundary_path = f"./collection/{boundary_type}/{k}"
         os.makedirs(boundary_path, exist_ok=True)
@@ -36,3 +46,6 @@ if __name__ == "__main__":
     for typ in boundary_files:
         filename = extract_name_from_document_url(typ['documentation-url'])
         split_boundaries(f"collection/{filename}.geojson",get_boundary_type(typ['type']))
+    
+
+    indexed_dict_to_csv("index/boundaries.csv", boundary_index, 'statistical-geography')
